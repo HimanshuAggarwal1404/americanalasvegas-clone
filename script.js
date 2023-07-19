9747869e25481df581fbf986882e1280732a5c48
Himanshu Aggarwal <120503620+HimanshuAggarwal1404@users.noreply.github.com>
2023-07-20 03:40:03 +0530

    section2 added
    animation yet to be added

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ function scrollFunction(){
     }
 }
 document.getElementById("video1").loop=true;
+
+const observer=new IntersectionObserver((entries)=>{
+entries.forEach((entry)=>{
+console.log(entry);
+if (entry.isIntersecting){
+    entry.target.classList.add("show");
+}
+});
+
+})
+
+const hiddenElements=document.querySelectorAll(".aboutus-img");
+hiddenElements.forEach((el)=>observer.observe(el));
