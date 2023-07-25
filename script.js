5652951561c5bdf61c9326c3ea7fb0b87008105c
Himanshu Aggarwal <120503620+HimanshuAggarwal1404@users.noreply.github.com>
2023-07-25 16:45:04 +0530

    reviews carousel added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let carouselPosition = 0;
+let reviewcarouselpos=0;
 var myInterval=null;
 window.onload=function(){
     playpause = 1; //stop
@@ -8,15 +9,85 @@ window.onload=function(){
         carouselPosition = carouselPosition == -300 ? 0 : carouselPosition - 100;
         setCarousel();
       }, 4000);
+      otherInterval = setInterval(() => {
+      reviewcarouselpos = reviewcarouselpos == -400 ? 0 : reviewcarouselpos - 100;
+      setreviewCarousel();
+      }, 10000);
 }
 window.onscroll = function () {
   scrollFunction();
 };
-let playpause = 1; //play
+let playpause = 1;
+document.getElementById("dot1").style.backgroundColor="white";
 function setCarousel() {
+  if(carouselPosition==0){
+    document.getElementById("dot1").style.backgroundColor="white";
+    document.getElementById("dot2").style.backgroundColor="";
+    document.getElementById("dot3").style.backgroundColor="";
+    document.getElementById("dot4").style.backgroundColor="";
+  }
+  else if(carouselPosition==-100){
+    document.getElementById("dot2").style.backgroundColor="white";
+    document.getElementById("dot1").style.backgroundColor="";
+    document.getElementById("dot3").style.backgroundColor="";
+    document.getElementById("dot4").style.backgroundColor="";
+  }
+  else if(carouselPosition==-200){
+    document.getElementById("dot3").style.backgroundColor="white";
+    document.getElementById("dot2").style.backgroundColor="";
+    document.getElementById("dot1").style.backgroundColor="";
+    document.getElementById("dot4").style.backgroundColor="";
+  }
+  else if(carouselPosition==-300){
+    document.getElementById("dot4").style.backgroundColor="white";
+    document.getElementById("dot2").style.backgroundColor="";
+    document.getElementById("dot3").style.backgroundColor="";
+    document.getElementById("dot1").style.backgroundColor="";
+  }
   document.getElementById(
     "carousel"
   ).style.transform = `translateX(${carouselPosition}vw)`;
+}
+function setreviewCarousel(){
+  if(reviewcarouselpos==0){
+    document.getElementById("reviews-controlbar-dot1").style.backgroundColor="#a31521";
+    document.getElementById("reviews-controlbar-dot2").style.backgroundColor="";
+    document.getElementById("reviews-controlbar-dot3").style.backgroundColor="";
+    document.getElementById("reviews-controlbar-dot4").style.backgroundColor="";
+    document.getElementById("reviews-controlbar-dot5").style.backgroundColor="";
+  }
+  else if(reviewcarouselpos==-100){
+    document.getElementById("reviews-controlbar-dot2").style.backgroundColor="#a31521";
+    document.getElementById("reviews-controlbar-dot1").style.backgroundColor="";
+    document.getElementById("reviews-controlbar-dot3").style.backgroundColor="";
+    document.getElementById("reviews-controlbar-dot4").style.backgroundColor="";
+    document.getElementById("reviews-controlbar-dot5").style.backgroundColor="";
+  }
+  else if(reviewcarouselpos==-200){
+    document.getElementById("reviews-controlbar-dot3").style.backgroundColor="#a31521";
+    document.getElementById("reviews-controlbar-dot1").style.backgroundColor="";
+    document.getElementById("reviews-controlbar-dot2").style.backgroundColor="";
+    document.getElementById("reviews-controlbar-dot4").style.backgroundColor="";
+    document.getElementById("reviews-controlbar-dot5").style.backgroundColor="";
+  }
+  else if(reviewcarouselpos==-300){
+    document.getElementById("reviews-controlbar-dot4").style.backgroundColor="#a31521";
+    document.getElementById("reviews-controlbar-dot1").style.backgroundColor="";
+    document.getElementById("reviews-controlbar-dot3").style.backgroundColor="";
+    document.getElementById("reviews-controlbar-dot2").style.backgroundColor="";
+    document.getElementById("reviews-controlbar-dot5").style.backgroundColor="";
+  }
+  else if(reviewcarouselpos==-400){
+    document.getElementById("reviews-controlbar-dot5").style.backgroundColor="#a31521";
+    document.getElementById("reviews-controlbar-dot1").style.backgroundColor="";
+    document.getElementById("reviews-controlbar-dot3").style.backgroundColor="";
+    document.getElementById("reviews-controlbar-dot2").style.backgroundColor="";
+    document.getElementById("reviews-controlbar-dot4").style.backgroundColor="";
+  }
+
+  document.getElementById(
+    "recarousel"
+  ).style.transform = `translateX(${reviewcarouselpos}vw)`;
 }
 document.getElementById("stopPlay").addEventListener("click", () => {
   if (playpause == 0) {
@@ -82,4 +153,24 @@ document.getElementById("dot3").addEventListener("click", () => {
 document.getElementById("dot4").addEventListener("click", () => {
   carouselPosition = -300;
   setCarousel();
+});
+document.getElementById("reviews-controlbar-dot1").addEventListener("click", () => {
+  reviewcarouselpos = 0;
+  setreviewCarousel();
+});
+document.getElementById("reviews-controlbar-dot2").addEventListener("click", () => {
+  reviewcarouselpos = -100;
+  setreviewCarousel();
+});
+document.getElementById("reviews-controlbar-dot3").addEventListener("click", () => {
+  reviewcarouselpos = -200;
+  setreviewCarousel();
+});
+document.getElementById("reviews-controlbar-dot4").addEventListener("click", () => {
+  reviewcarouselpos = -300;
+  setreviewCarousel();
+});
+document.getElementById("reviews-controlbar-dot5").addEventListener("click", () => {
+  reviewcarouselpos = -400;
+  setreviewCarousel();
 });
